50afaccde34c800a11ddac0cb2342691877358a5
kareldebrouwere <kareldebrouwere@gmail.com>
2017-03-02 20:05:27 +0100

    yes

--- webserver.py
+++ webserver.py
@@ -39,8 +39,8 @@ class MotorWebServer(object):
     def start(self):
         print("the clockwise1 method start")
         myMotor = motor.Motor()
-        myMotor.turnClockWise()
-        return "Done"
+        myMotor.run()
+        return self.index()
 
 if __name__ == "__main__":
     config = os.path.join(os.path.dirname(__file__),'cherrypy.conf')
