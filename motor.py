6d6eef9c9a0ee11f724b35fbb57834ffdc0dba98
kareldebrouwere <kareldebrouwere@gmail.com>
2017-03-02 18:52:05 +0100

    yes

--- motor.py
+++ motor.py
@@ -3,67 +3,67 @@
 import sys
 import time
 import RPi.GPIO as GPIO
+
+
+class Motor(object):
+
+    def __init__(self):
+        # Use BCM GPIO references
+        # instead of physical pin numbers
+        GPIO.setmode(GPIO.BCM)
  
-# Use BCM GPIO references
-# instead of physical pin numbers
-GPIO.setmode(GPIO.BCM)
+        # Define GPIO signals to use
+        # Physical pins 11,15,16,18
+        # GPIO17,GPIO22,GPIO23,GPIO24
+        self.StepPins = [17,22,23,24]
+
+        # Set all pins as output
+        for pin in self.StepPins:
+            print ("Setup pins")
+            GPIO.setup(pin,GPIO.OUT)
+            GPIO.output(pin, False)
+
+        # Define advanced sequence
+        # as shown in manufacturers datasheet
+        self.Seq = [[1,0,0,1],
+               [1,0,0,0],
+               [1,1,0,0],
+               [0,1,0,0],
+               [0,1,1,0],
+               [0,0,1,0],
+               [0,0,1,1],
+               [0,0,0,1]]
+
+        self.StepCount = len(Seq)
+        self.StepDir = 1 # Set to 1 or 2 for clockwise
+                    # Set to -1 or -2 for anti-clockwise
  
-# Define GPIO signals to use
-# Physical pins 11,15,16,18
-# GPIO17,GPIO22,GPIO23,GPIO24
-StepPins = [17,22,23,24]
+        self.WaitTime = 10/float(1000)
  
-# Set all pins as output
-for pin in StepPins:
-    print ("Setup pins")
-    GPIO.setup(pin,GPIO.OUT)
-    GPIO.output(pin, False)
- 
-# Define advanced sequence
-# as shown in manufacturers datasheet
-Seq = [[1,0,0,1],
-       [1,0,0,0],
-       [1,1,0,0],
-       [0,1,0,0],
-       [0,1,1,0],
-       [0,0,1,0],
-       [0,0,1,1],
-       [0,0,0,1]]
-        
-StepCount = len(Seq)
-StepDir = 1 # Set to 1 or 2 for clockwise
-            # Set to -1 or -2 for anti-clockwise
- 
-# Read wait time from command line
-if len(sys.argv)>1:
-    WaitTime = int(sys.argv[1])/float(1000)
-else:
-    WaitTime = 10/float(1000)
- 
-# Initialise variables
-StepCounter = 0
- 
-# Start main loop
-while True:
- 
-  print (StepCounter)
-  print (Seq[StepCounter])
-  for pin in range(0,4):
-    xpin=StepPins[pin]# Get GPIO
-    if Seq[StepCounter][pin]!=0:
-      print (" Enable GPIO %i" %(xpin))
-      GPIO.output(xpin, True)
-    else:
-      GPIO.output(xpin, False)
- 
-  StepCounter += StepDir
- 
-  # If we reach the end of the sequence
-  # start again
-  if (StepCounter>=StepCount):
-    StepCounter = 0
-  if (StepCounter<0):
-    StepCounter = StepCount+StepDir
- 
-  # Wait before moving on
-  time.sleep(WaitTime/50)
+        # Initialise variables
+        StepCounter = 0
+
+    def turnClockWise(self):
+        while True:
+
+          print (self.StepCounter)
+          print (self.Seq[self.StepCounter])
+          for pin in range(0,4):
+            xpin=self.StepPins[pin]# Get GPIO
+            if self.Seq[self.StepCounter][pin]!=0:
+              print (" Enable GPIO %i" %(xpin))
+              GPIO.output(xpin, True)
+            else:
+              GPIO.output(xpin, False)
+
+          self.StepCounter += self.StepDir
+
+          # If we reach the end of the sequence
+          # start again
+          if (self.StepCounter>=self.StepCount):
+            self.StepCounter = 0
+          if (self.StepCounter<0):
+            self.StepCounter = self.StepCount+self.StepDir
+
+          # Wait before moving on
+          time.sleep(self.WaitTime/50)
