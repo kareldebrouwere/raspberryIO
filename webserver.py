a265337e9c5d244f3fe41df69b0b7e6e64412dce
kareldebrouwere <kareldebrouwere@gmail.com>
2017-03-06 19:52:14 +0100

    yes

--- webserver.py
+++ webserver.py
@@ -4,20 +4,22 @@ import motor
 import logging
 
 HTML= """<html>
-          <head><style>
-        .button {
-             background-color: #4CAF50;
-            border: none;
-            color: white;
-            padding: 15px 32px;
-             text-align: center;
-             text-decoration: none;
-             display: inline-block;
-            font-size: 16px;
-             margin: 4px 2px;
-             cursor: pointer;
-            }
-            </style></head>
+          <head>
+          <style>
+                .button {
+                background-color: #4CAF50;
+                border: none;
+                color: white;
+                padding: 15px 32px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                margin: 4px 2px;
+                cursor: pointer;
+                }
+        </style>
+        </head>
           <body>
               {button1}
               {button2}
